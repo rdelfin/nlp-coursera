1e8235d6f57afbe161d724ef4d9f3a8b7369c86f
Ricardo Delfin Garcia <ricardo.delfin.garcia@gmail.com>
2016-05-08 00:37:11 -0500

    Added classes in Data.hpp

--- include/assignment1/Data.hpp
+++ include/assignment1/Data.hpp
@@ -4,7 +4,24 @@
 
 #pragma once
 
+#include <string>
+#include <vector>
+
+typedef enum {
+    O,
+    I_GENE,
+    START,
+    END
+} Tag;
+
 class Word {
 public:
-private:
+    int count;
+    Tag tag;
+    std::string name;
+};
+
+class Ngram {
+public:
+    std::vector<Tag> terms;
 };
--- src/Data.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Ricardo Delfin Garcia on 5/8/16.
-//
-
-#include "assignment1/Data.hpp"
